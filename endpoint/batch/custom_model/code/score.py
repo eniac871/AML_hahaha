1c944755f297b61511c8883179d4edae41722b54
Anthony Hu <supereniac@live.com>
2022-08-15 10:46:14 +0000

    update score.py

--- endpoint/batch/custom_model/code/score.py
+++ endpoint/batch/custom_model/code/score.py
@@ -5,7 +5,7 @@ import xgboost as xgb
 from PIL import Image
 import pandas as pd
 import mlflow
-from azureml.core import Model
+#from azureml.core import Model
 
 
 def init():
@@ -16,7 +16,7 @@ def init():
     # Please provide your model's folder name if there's one
     model_path = os.path.join(os.environ["AZUREML_MODEL_DIR"], "model.json")
 
-    # model_path = "../model/model.json"
+    # model_path = "f:/AML_HAHAHA/model.json"
     #mlflow.log_text( os.listdir(os.environ["AZUREML_MODEL_DIR"]), "custome_log.txt")
     xgb_model = xgb.Booster()
     xgb_model.load_model(model_path)
@@ -30,8 +30,56 @@ def run(mini_batch):
 
     for line in mini_batch:
         #mlflow.log_text(line, "minibatch.txt")
-        data_to_test = pd.read_csv(line)
-        xgbdata = xgb.DMatrix(data_to_test)
+        test = pd.read_csv(line)
+
+        # data cleanup
+        test.drop("Id", axis=1, inplace=True)
+        data = test.reset_index(drop=True)
+        #data.drop(['SalePrice'], axis=1, inplace = True)
+
+        # features that can use the most common data or things means average to fillna
+        data['Functional'] = data['Functional'].fillna('Typ')
+        data['Electrical'] = data['Electrical'].fillna('SBrkr')
+        data['KitchenQual'] = data['KitchenQual'].fillna('TA')
+        data['Exterior1st'] = data['Exterior1st'].fillna(data['Exterior1st'].mode()[0])
+        data['Exterior2nd'] = data['Exterior2nd'].fillna(data['Exterior2nd'].mode()[0])
+
+        # show then remove house without pool but has poorArea
+        pd.options.display.max_columns=None
+        data[data['PoolArea'] > 0 & data['PoolQC'].isnull()][["PoolArea", "PoolQC"]]
+
+        # data.loc[2418, 'PoolQC'] = 'Fa'
+        # data.loc[2501, 'PoolQC'] = 'Gd'
+        # data.loc[2597, 'PoolQC'] = 'Fa'
+        data[(data['GarageYrBlt']>2022)]
+
+        location = data[data['GarageYrBlt']>2022]
+        location
+        data.loc[data['GarageYrBlt']>2022,'GarageYrBlt'] = 2007
+
+        pd.options.display.max_columns=None
+        pd.options.display.max_rows=None
+        data[data['GarageYrBlt'].notnull() & data['GarageType'].isnull()][['GarageYrBlt', "GarageType"]]
+
+        temp = data[data['MSZoning'].isnull()]
+        print("NA counts for MSZoning is {}", format(temp.shape[0]))
+        # temporarily use mode to fill MSZoning NA accordiing to mssubclass
+        msclass_group = data.groupby('MSSubClass')
+        zoning = msclass_group['MSZoning'].apply(lambda x :x.mode()[0])
+        data['MSZonging'] = data.groupby('MSSubClass')['MSZoning'].transform(lambda x: x.fillna(x.mode()[0]))
+
+        i=0
+        #while i < data.shape[1]:
+            #data.iloc[:,i].fillna(data.iloc[:,i].mode()[0])
+            #i +=1
+        while i < data.shape[1]:
+            data.loc[:, data.columns[i]] = data[data.columns[i]].fillna(data.iloc[:,i].mode()[0])
+            i = i+1
+        test_dummies = pd.get_dummies(data).reset_index(drop=True)
+
+
+
+        xgbdata = xgb.DMatrix(test_dummies)
         test_predict = xgb_model.predict(xgbdata)
         resultList += test_predict.tolist()
     return resultList
@@ -51,8 +99,9 @@ def run(mini_batch):
     # return resultList
 
 # def predict_data():
-#     testdata = pd.read_csv("../../endpoint_data/test_dummies.csv")
-#     run(testdata)
+#     # testdata = pd.read_csv("../../endpoint_data/test_dummies.csv")
+#     run(["F:\AML_hahaha\data\house-prices-advanced-regression-techniques\\test.csv"])
+
 
 # init()
 
